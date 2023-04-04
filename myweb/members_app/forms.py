52452dc4fba9e1640be5d721fd8871a201adea7b
Angelina Boryn <aboryn@gmail.com>
2023-04-04 22:50:15 +0200

    updated widgets in forms

---
+++ myweb/members_app/forms.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterUserForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=60, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=60, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def __int__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'class-control'
+        self.fields['password1'].widget.attrs['class'] = 'class-control'
+        self.fields['password2'].widget.attrs['class'] = 'class-control'
--- myweb/members_app/views.py
+++ myweb/members_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from .forms import RegisterUserForm
 
 
 def login_user(request):
@@ -11,6 +14,7 @@ def login_user(request):
         if user is not None:
             login(request, user)
             # Redirect to a success page
+            messages.success(request, "You are logged in")
             return redirect('home')
         else:
             messages.success(request, 'There was wrong login, Try again')
@@ -23,5 +27,21 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.success(request, 'You are logout')
+    messages.success(request, 'You are logged out')
     return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Register was successful")
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+    return render(request, "auth/register_user.html", {"form": form})
